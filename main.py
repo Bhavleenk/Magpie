395a2ab41527787fc4e2c99f853347e00830df3c
Bhavleenk <bhavcode@gmail.com>
2023-03-27 16:13:07 +0530

    Magpie-SudokuSolver

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ grid = [
 
 
 def possible(x, y, n):  # n is the possible number
-    for i in range(0,9):
+    for i in range(0,9): #check col
         if grid[i][x]==n and i!=y:
             return False
-    for i in range(0,9):
+    for i in range(0,9): #check row
        if grid[y][i] !=n and i!=x:
             return False
+
+    x0 = (x//3)*3
+    y0=(x//3)*3
+    for X in range(x0,y0+3): #check box
+        for Y in range(y0,y0+3):
+            if grid[Y][X]==n:
+                return False
+    return True
