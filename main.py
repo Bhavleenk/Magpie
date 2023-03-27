0fca230d0a0180713eb9b5bc798f0d44cde767d8
Bhavleenk <bhavcode@gmail.com>
2023-03-27 16:03:56 +0530

    Magpie-SudokuSolver

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+grid = [
+    [4, 0, 0, 0, 0, 5, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 9, 8],
+    [3, 0, 0, 0, 8, 2, 4, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 8, 0],
+    [9, 0, 3, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 6, 7, 0],
+    [0, 5, 0, 0, 0, 9, 0, 0, 0],
+    [0, 0, 0, 2, 0, 0, 9, 0, 7],
+    [6, 4, 0, 3, 0, 0, 0, 0, 0]]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def possible(x, y, n):  # n is the possible number
