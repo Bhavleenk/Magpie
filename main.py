88e31ef6117017963b6d72d9b4bc6c36b995c5b1
Bhavleenk <bhavcode@gmail.com>
2023-03-27 16:25:02 +0530

    Magpie-SudokuSolver

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import numpy as np
 
-grid = []
+grid = [
+    [4, 0, 0, 0, 0, 5, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 9, 8],
+    [3, 0, 0, 0, 8, 2, 4, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 8, 0],
+    [9, 0, 3, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 6, 7, 0],
+    [0, 5, 0, 0, 0, 9, 0, 0, 0],
+    [0, 0, 0, 2, 0, 0, 9, 0, 7],
+    [6, 4, 0, 3, 0, 0, 0, 0, 0]]
 
-while True:
+'''while True:
     row = list(input('Row: '))
     ints = []
 
@@ -12,7 +21,7 @@ while True:
 
     if len(grid) == 9:
         break
-    print('Row ' + str(len(grid)) + ' Complete')
+    print('Row ' + str(len(grid)) + ' Complete')'''
 
 
 def possible(x, y, n):
@@ -47,3 +56,8 @@ def solve():
     input("More?")
 
 solve()
+
+
+
+
+
